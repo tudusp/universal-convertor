9e68806ece346cd63deea99e5755956f26756d59
tudusp <140788767+tudusp@users.noreply.github.com>
2025-06-22 21:42:57 +0530

    Update app.py

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ import tabula
 # Page configuration
 st.set_page_config(
     page_title="Universal Convertor",
-    page_icon="🍕",
+    page_icon="icon.png",
     layout="wide",
     initial_sidebar_state="expanded"
 )
